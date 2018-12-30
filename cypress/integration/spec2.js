f777fbd65f67990e56815a9bcb2ffb5b35f229e1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-30 09:14:53 -0500

    more testing

---
+++ cypress/integration/spec2.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+it('loads 3 elements', () => {
+  cy.visit('index.html')
+  cy.get('#app div')
+    .should(($div) => {
+      expect($div.eq(0)).to.contain('first child')
+      expect($div.eq(1)).to.contain('second child')
+      expect($div.eq(2)).to.contain('third child')
+    })
+})
